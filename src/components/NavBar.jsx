6d2224d5fb4332ed667c95e5cc3e357af77373a5
anajbsouza <anaju.batsouza@gmail.com>
2023-05-29 15:52:04 -0300

    data-test d novo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styled from 'styled-components';
 
 export default function NavBar() {
+    const navigate = useNavigate();
+
     return (
         <NavContainer>
             <Link to="/">
--- src/components/SessionCard.jsx
+++ src/components/SessionCard.jsx
@@ -9,8 +9,8 @@ export default function SessionCard({ days }) {
             <div data-test="movie-day">{weekday} - {date}</div>
             <ButtonsContainer>
             {showtimes.map(({ id, name: hour }) => (
-                <Link to={`/assentos/${id}`} key={id}>
-                    <button data-test="showtime">{hour}</button>
+                <Link to={`/assentos/${id}`} key={id} data-test="showtime">
+                    <button>{hour}</button>
                 </Link>
             ))}
             </ButtonsContainer>
